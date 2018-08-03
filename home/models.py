fb34a68d638a527a80627b4afa47f97b648ea376
João Gabriel A. Caribé <joaogcaribe@gmail.com>
2018-08-03 15:41:06 -0300

    Página principal adicionada mas pouco funcional.

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from login.models import CustomUser
 
 # Create your models here.
+class Course(models.Model):
+	id = models.AutoField( primary_key = True)
+	professor = models.ForeignKey( CustomUser, on_delete = models.CASCADE )
+	name = models.CharField( max_length = 100 , default = "sem_nome")
+	
+	def __str__(self):
+		return self.name
--- home/views.py
+++ home/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from home.models import Course
 
 # Create your views here.
+@login_required
+def main(request):
+	course_list = Course.objects.filter(professor = request.user) #Busca por todos os cursos ministrados pelo professor logado
+	context = { 'course_list': course_list} #Prepara a informação para a página
+	return render(request, 'home/main.html', context)
